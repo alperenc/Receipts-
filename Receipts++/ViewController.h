3c046a8a955a9c06be279ca6f54b1a88ac40de5b
Alp Eren Can <alperenc@gmail.com>
2015-09-17 12:20:01 -0700

    Initial Commit

---
+++ Receipts++/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Receipts++
+//
+//  Created by Alp Eren Can on 17/09/15.
+//  Copyright © 2015 Alp Eren Can. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
